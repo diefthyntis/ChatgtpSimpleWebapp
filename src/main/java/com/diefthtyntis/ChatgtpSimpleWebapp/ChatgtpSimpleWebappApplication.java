58efc645589ce6e727e2041cab1961aab81c79ca
Alan Hauser <diefthyntis@gmail.com>
2024-08-05 11:26:59 +0200

    Premier commit

---
+++ src/main/java/com/diefthtyntis/ChatgtpSimpleWebapp/ChatgtpSimpleWebappApplication.java
@@ -0,0 +1,13 @@
+package com.diefthtyntis.ChatgtpSimpleWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgtpSimpleWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgtpSimpleWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diefthtyntis/ChatgtpSimpleWebapp/ChatgtpSimpleWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diefthtyntis.ChatgtpSimpleWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgtpSimpleWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
